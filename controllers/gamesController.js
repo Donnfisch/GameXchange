63d98e4f78d5beb9743709d684ef18f60d0702dd
1701c <alex@reventlow.net>
2019-03-08 13:47:32 -0600

    backend and frontend loading together

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -2,10 +2,11 @@ const db = require("../models");
 
 // Defining methods for the gamesController
 module.exports = {
-  
+
   // Lists all games
   findAll: (req, res) => {
-    db.Game.findAll({ where: {status: "approved"}, limit:50
+    db.Game.findAll({
+      where: { status: "approved" }, limit: 50
     }).then(dbGames => {
       res.json(dbGames);
     });
@@ -15,13 +16,14 @@ module.exports = {
   findById: (req, res) => {
     db.Game.findByPk(req.params.id)
       .then(dbGame => {
-      res.json(dbGame);
-    });
+        res.json(dbGame);
+      });
   },
 
   // Search games by title
   findByTitle: (req, res) => {
-    db.Game.findAll({ where: {title: { $like: '%' + req.params.title + '%'}}, limit:50
+    db.Game.findAll({
+      where: { title: { $like: '%' + req.params.title + '%' } }, limit: 50
     }).then(dbGames => {
       res.json(dbGames);
     });
@@ -29,13 +31,25 @@ module.exports = {
 
   // Returns user's games along with have/want/trade status
   myGames: (req, res) => {
-    db.sequelize.query(`SELECT id, userId, w.gameId, have, want, trade, g.platform, g.title, g.region, g.publisher, g.version FROM wishLists w
-    LEFT JOIN
-    (SELECT * FROM games) as g
-    ON (g.gameId = w.gameId) WHERE userId = 1`, { type: db.sequelize.QueryTypes.SELECT})
-    .then(dbGames => {
-      res.json(dbGames);
-    });
+    db.sequelize.query(`SELECT id, 
+        userid, 
+        w.gameid, 
+        have, 
+        want, 
+        trade, 
+        g.platform, 
+        g.title, 
+        g.region, 
+        g.publisher, 
+        g.version 
+    FROM   wishlists w 
+        LEFT JOIN (SELECT * 
+                  FROM   games) AS g 
+              ON ( g.gameid = w.gameid ) 
+    WHERE  userid = 1`, { type: db.sequelize.QueryTypes.SELECT })
+      .then(dbGames => {
+        res.json(dbGames);
+      });
   },
 
   // Matches users trades with others
@@ -71,30 +85,29 @@ module.exports = {
                       u.email, 
                       m.mygameid 
                 FROM   users u 
-                      INNER JOIN (SELECT A.gameid AS myGameId, 
-                                          B.gameid AS theirGameId, 
-                                          A.userid AS myUserId, 
-                                          B.userid AS theirUserId, 
-                                          A.want   AS want, 
-                                          B.have   AS have 
-                                  FROM    wishLists A, 
-                                          wishLists B 
-                                  WHERE  A.want <> 0 
-                                            AND B.have <> 0 
-                                            AND A.gameid = B.gameid 
-                                            AND A.userid ${userIdACompare} ${currentUser} 
-                                            AND B.userid ${userIdBCompare} ${currentUser}) AS m 
-                                  ON ( u.userid = ${userToMatch} )) AS h 
-                ON ( g.gameid = h.mygameid )
-                ORDER BY email`, { type: db.sequelize.QueryTypes.SELECT })
+                INNER JOIN (SELECT A.gameid AS myGameId, 
+                                    B.gameid AS theirGameId, 
+                                    A.userid AS myUserId, 
+                                    B.userid AS theirUserId, 
+                                    A.want   AS want, 
+                                    B.have   AS have 
+                            FROM    wishLists A, 
+                                    wishLists B 
+                            WHERE  A.want <> 0 
+                                    AND B.have <> 0 
+                                    AND A.gameid = B.gameid 
+                                    AND A.userid ${userIdACompare} ${currentUser} 
+                                    AND B.userid ${userIdBCompare} ${currentUser}) AS m 
+                            ON ( u.userid = ${userToMatch} )) AS h 
+          ON ( g.gameid = h.mygameid )
+          ORDER BY email`, { type: db.sequelize.QueryTypes.SELECT })
       .then(dbGames => {
         res.json(dbGames);
       });
   },
 
   // Create game
-  create: function(req, res) {
-    console.log(req.body);
+  create: function (req, res) {
     req.body.status = "requested";
     db.Game.create(req.body)
       .then(dbGame => res.json(dbGame))
