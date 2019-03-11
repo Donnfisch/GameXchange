72970da7eaa4beccc4d5fc39a2077d0f4916199e
1701c <alex@reventlow.net>
2019-03-11 12:39:00 -0500

    SQL Query cleanup

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -6,7 +6,8 @@ module.exports = {
   // TODO: Need to catch currentUser
   findAll: (req, res) => {
     const currentUser = "5272e292-3c40-4eea-a3df-707b760fdf00";
-    db.sequelize.query(`SELECT w.id, 
+    db.sequelize.query(
+      `SELECT w.id, 
         w.userid, 
         w.gameid, 
         have, 
@@ -17,11 +18,11 @@ module.exports = {
         g.region, 
         g.publisher, 
         g.version 
-    FROM   inventories w 
-        LEFT JOIN (SELECT * 
-                  FROM   games) AS g 
-              ON ( g.id = w.gameid ) 
-    WHERE  w.userid = "${currentUser}"`, { type: db.sequelize.QueryTypes.SELECT })
+      FROM inventories w 
+      LEFT JOIN (SELECT * FROM games) AS g 
+        ON ( g.id = w.gameid ) 
+      WHERE  w.userid = "${currentUser}"`, { type: db.sequelize.QueryTypes.SELECT }
+    )
       .then(dbGames => {
         res.json(dbGames);
       });
@@ -39,43 +40,45 @@ module.exports = {
       userIdBCompare = "!=";
       userToMatch = "m.theirUserId";
     }
-    db.sequelize.query(`SELECT g.id, 
-      g.title, 
-      g.platform, 
-      g.region, 
-      g.publisher, 
-      g.version, 
-      h.id, 
-      h.username, 
-      h.firstname, 
-      h.lastname, 
-      h.address, 
-      h.email 
-    FROM games g 
-    INNER JOIN (SELECT u.id, 
-                      u.username, 
-                      u.firstname, 
-                      u.lastname, 
-                      u.address, 
-                      u.email, 
-                      m.mygameid 
-                FROM   users u 
-                INNER JOIN (SELECT A.gameid AS myGameId, 
-                                    B.gameid AS theirGameId, 
-                                    A.userid AS myUserId, 
-                                    B.userid AS theirUserId, 
-                                    A.want   AS want, 
-                                    B.have   AS have 
-                            FROM    inventories A, 
-                                    inventories B 
-                            WHERE  A.want <> 0 
-                                    AND B.have <> 0 
-                                    AND A.gameid = B.gameid 
-                                    AND A.userid ${userIdACompare} "${currentUser}" 
-                                    AND B.userid ${userIdBCompare} "${currentUser}") AS m 
-                            ON ( u.id = ${userToMatch} )) AS h 
-          ON ( g.id = h.mygameid )
-          ORDER BY email;`, { type: db.sequelize.QueryTypes.SELECT })
+    db.sequelize.query(
+      `SELECT g.id, 
+        g.title, 
+        g.platform, 
+        g.region, 
+        g.publisher, 
+        g.version, 
+        h.id, 
+        h.username, 
+        h.firstname, 
+        h.lastname, 
+        h.address, 
+        h.email 
+      FROM games g 
+      INNER JOIN (SELECT u.id, 
+                    u.username, 
+                    u.firstname, 
+                    u.lastname, 
+                    u.address, 
+                    u.email, 
+                    m.mygameid 
+                  FROM   users u 
+                  INNER JOIN (SELECT A.gameid AS myGameId, 
+                                B.gameid AS theirGameId, 
+                                A.userid AS myUserId, 
+                                B.userid AS theirUserId, 
+                                A.want   AS want, 
+                                B.have   AS have 
+                              FROM inventories A, 
+                                inventories B 
+                              WHERE  A.want <> 0 
+                                AND B.have <> 0 
+                                AND A.gameid = B.gameid 
+                                AND A.userid ${userIdACompare} "${currentUser}" 
+                                AND B.userid ${userIdBCompare} "${currentUser}") AS m 
+                    ON ( u.id = ${userToMatch} )) AS h 
+        ON ( g.id = h.mygameid )
+      ORDER BY email;`, { type: db.sequelize.QueryTypes.SELECT }
+    )
       .then(dbGames => {
         res.json(dbGames);
       });
