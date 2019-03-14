87859d8060358fda357561a75e6db117eff6b9ea
1701c <alex@reventlow.net>
2019-03-14 14:03:03 -0500

    upsertOrDelete route and logic

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -16,14 +16,6 @@ module.exports = {
     });
   },
 
-  // findAll: (req, res) => {
-  //   const currentUser = "5272e292-3c40-4eea-a3df-707b760fdf00";
-  //   db.inventory.findUserInventory(currentUser)
-  //     .then(dbInventory => {
-  //       res.json(dbInventory);
-  //     });
-  // },
-
   // Matches users trades with others
   // TODO: Need to catch currentUser
   findMatches: (req, res) => {
@@ -32,10 +24,19 @@ module.exports = {
       where: {
         trade: true,
       },
-      attributes: ['id', 'trade'],
+      attributes: [
+        'id',
+        'trade',
+      ],
       include: [{
         model: db.game,
-        attributes: ['id', 'title', 'platform', 'publisher', 'version'],
+        attributes: [
+          'id',
+          'title',
+          'platform',
+          'publisher',
+          'version',
+        ],
         required: true,
         include: [{
           model: db.inventory,
@@ -48,7 +49,15 @@ module.exports = {
           attributes: ['want'],
           include: [{
             model: db.user,
-            attributes: ['id', 'username', 'email', 'firstname', 'lastname', 'address', 'email'],
+            attributes: [
+              'id',
+              'username',
+              'email',
+              'firstname',
+              'lastname',
+              'address',
+              'email',
+            ],
           }],
         }],
       }],
@@ -57,24 +66,44 @@ module.exports = {
     });
   },
 
-
-  // findMatches: (req, res) => {
-  //   const currentUser = "5272e292-3c40-4eea-a3df-707b760fdf00";
-  //   db.inventory.findMatches(currentUser, req.params.direction)
-  //     .then(dbMatches => {
-  //       res.json(dbMatches);
-  //     });
-  // },
-
   // Add or update inventory items
   // TODO: catch currentUser
-  // TODO: Pull ADD UUID to REACT to gen unique IDs
-  // TODO: Create logic
   // TOTO: Bless the rains down in Africa
-  insert: (req, res) => {
+  upsertOrDelete: (req, res) => {
     // const currentUser = "5272e292-3c40-4eea-a3df-707b760fdf00";
-    db.inventory.create(req.body)
-      .then(dbInventory => res.json(dbInventory))
-      .catch(err => res.status(422).json(err));
+    db.inventory.findOne({
+      where: {
+        userId: req.body.userId,
+        gameId: req.body.gameId,
+      },
+    }).then(dbUpdate => {
+      if (dbUpdate) {
+        if (req.body.have === "false" && req.body.want === "false" && req.body.trade === "false") {
+          db.inventory.destroy({
+            where: {
+              userId: req.body.userId,
+              gameId: req.body.gameId,
+            },
+          }).then(dbInventory => res.json(dbInventory))
+            .catch(err => res.status(422).json(err));
+        } else {
+          db.inventory.update(
+            req.body, {
+              where: {
+                userId: req.body.userId,
+                gameId: req.body.gameId,
+              },
+            }
+          ).then(dbInventory => res.json(dbInventory))
+            .catch(err => res.status(422).json(err));
+        }
+      } else if (req.body.have === "true" || req.body.want === "true" || req.body.trade === "true") {
+        db.inventory.create(req.body)
+          .then(dbInventory => res.json(dbInventory))
+          .catch(err => res.status(422).json(err));
+      } else {
+        res.json("Nothing added to DB");
+      }
+    });
   },
 };
