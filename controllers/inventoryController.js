50333050c1be00e0bc38de18ae035a13420ab8e0
1701c <alex@reventlow.net>
2019-03-14 10:52:46 -0500

    removed unneeded fields from match api

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -6,9 +6,11 @@ module.exports = {
   // TODO: Need to catch currentUser
   findAll: (req, res) => {
     const currentUser = "5272e292-3c40-4eea-a3df-707b760fdf00";
-    db.inventory.findAll({
-      where: { userId: currentUser },
-      include: [db.game],
+    db.game.findAll({
+      include: [{
+        model: db.inventory,
+        where: { userId: currentUser },
+      }],
     }).then(dbInventory => {
       res.json(dbInventory);
     });
@@ -26,11 +28,43 @@ module.exports = {
   // TODO: Need to catch currentUser
   findMatches: (req, res) => {
     const currentUser = "5272e292-3c40-4eea-a3df-707b760fdf00";
-    db.inventory.findMatches(currentUser, req.params.direction)
-      .then(dbMatches => {
-        res.json(dbMatches);
-      });
+    db.inventory.findAll({
+      where: {
+        trade: true,
+      },
+      attributes: ['id', 'trade'],
+      include: [{
+        model: db.game,
+        attributes: ['id', 'title', 'platform', 'publisher', 'version'],
+        required: true,
+        include: [{
+          model: db.inventory,
+          where: {
+            userId: {
+              $not: currentUser,
+            },
+            want: true,
+          },
+          attributes: ['want'],
+          include: [{
+            model: db.user,
+            attributes: ['id', 'username', 'email', 'firstname', 'lastname', 'address', 'email'],
+          }],
+        }],
+      }],
+    }).then(dbInventory => {
+      res.json(dbInventory);
+    });
   },
+
+
+  // findMatches: (req, res) => {
+  //   const currentUser = "5272e292-3c40-4eea-a3df-707b760fdf00";
+  //   db.inventory.findMatches(currentUser, req.params.direction)
+  //     .then(dbMatches => {
+  //       res.json(dbMatches);
+  //     });
+  // },
 
   // Add or update inventory items
   // TODO: catch currentUser
