17642215b3ca39caf2911525ab8db1673565cdca
Shane Lennon <shane.michael.lennon@gmail.com>
2019-03-15 23:45:31 -0500

    Added logger and [assport

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+const logger = require("morgan");
 const express = require("express");
 const routes = require("./routes");
-const app = express();
-const PORT = process.env.PORT || 3001;
 const db = require("./models");
+const passport = require('./routes/api/passport');
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 
+// Configure middleware
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
+  // eslint-disable-next-line no-console
+  console.log("WTF!");
   app.use(express.static("client/build"));
 }
-
+app.use(passport);
 app.use(routes);
 
 db.sequelize.sync({ force: false }).then(() => {
