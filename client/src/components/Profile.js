8b66fc8fa46366c3e38e3290f912d46ba8fdf566
jeybone <jeysonp@gmail.com>
2019-03-25 20:07:05 -0500

    added Profile page initial content

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import profilePic from "./styles/images/profile.png";
+import './styles/Profile.css';
 
 export class Profile extends Component {
   render() {
     return (
-      <div>
-        Profile Component
+      <div class="ui card">
+        <div class="image">
+        <img src={profilePic} alt="profile" />
+        </div>
+        <div class="content">
+          <a class="header">Matthew</a>
+          <div class="meta">
+            <span class="date">Joined in 2019</span>
+          </div>
+          <div class="description">
+            Matthew is a game collector living in Kansas City.
+          </div>
+        </div>
+        <div class="extra content">
+        <button class="ui button">Request</button>
+        <button class="ui button">Offer</button>
+        </div>
       </div>
     );
   }
