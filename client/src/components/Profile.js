8d82ab5a60eeb6f87cdffc3dc99901710103a3d3
jeybone <jeysonp@gmail.com>
2019-03-27 18:46:23 -0500

    more changes to profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -5,38 +5,40 @@ import './styles/Profile.css';
 export class Profile extends Component {
   render() {
     return (
-      <div class="ui three column grid">
-        <div class="column">
-          <div class="ui fluid card">
-            <div class="image">
-              <img src={profilePic} alt="profile" />
-            </div>
-            <div class="content">
-              <a class="header">Leif Erickson</a>
-              <div class="meta">
-                <span class="date">Joined in 2019</span>
+      <div class="container">
+        <div class="ui three column grid">
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="image">
+                <img src={profilePic} alt="profile" />
               </div>
-              <div class="description">
-                Leif is a game collector living in Norway.
+              <div class="content">
+                <a class="header">Matt</a>
+                <div class="meta">
+                  <span class="date">Joined in 2019</span>
+                </div>
+                <div class="description">
+                  Matt is a game collector living in Kansas City.
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="column">
-          <div class="ui fluid card">
-            <div class="content">
-              <a class="header">More Stuff about the user</a>
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="content">
+                <a class="header">More Stuff about the user</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="column">
-          <div class="ui fluid card">
-            <div class="content">
-              <a class="header">and even more stuff</a>
+          <div class="column">
+            <div class="ui fluid card">
+              <div class="content">
+                <a class="header">and even more stuff</a>
+              </div>
             </div>
-          </div>
+        </div>
       </div>
-      </div>
+    </div> 
     );
   }
 }
