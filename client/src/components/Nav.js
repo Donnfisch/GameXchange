865eaf74ab9e1b36476e17e9cca5378b46e34978
donnfisch <mrblix@gmail.com]>
2019-03-29 23:38:01 -0500

    testing

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,43 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, InputGroup, InputGroupButtonDropdown, Button, DropdownToggle, DropdownMenu, DropdownItem, Input, InputGroupAddon } from 'reactstrap';
-import navLogo from '../img/GXnavlogo.png';
-import '../Style/navBar.css';
-import LogIn from "./LogIn";
-import Search from "./Search";
-import NavLinks from "./NavLinks";
-
-
-
-const nLogo = [navLogo];
-
-
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  }
-
-
-=======
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Menu, Image } from 'semantic-ui-react';
 import Search from "./Search";
 import NavLinks from "./NavLinks";
 import LogIn from "./LogIn";
 import navLogo from "./styles/images/GXnavlogo.png";
-import Welcome from "./Welcome";
 import "./styles/Navbar.css";
 
 export default class Nav extends Component {
@@ -45,55 +11,35 @@ export default class Nav extends Component {
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
->>>>>>> master
   render() {
     const {
       handleSearch, handleMyGames, handleMatches, history, setUserState,
     } = this.props;
     const { activeItem } = this.state;
+
     return (
-<<<<<<< HEAD
-      <div>
-        <Navbar color="#b503c5" light expand="sm">
-          <NavbarBrand href="/">
-            <img src={nLogo} alt="Logo" />
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavLinks />
-              <Search />
-              <LogIn />
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-=======
       <Menu secondary>
-        <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick}>
+        <Menu.Item>
           <Image
             src={navLogo}
             size="small"
             as="a"
-            href={Welcome}
-            target="_blank"
+            href="/"
           />
         </Menu.Item>
         <Menu.Item>
-          <Search handleSearch={handleSearch} />
+          <Search className="navBarComp" handleSearch={this.props.handleSearch} />
         </Menu.Item>
         <Menu.Item>
-          <NavLinks
-            history={history}
-            handleMyGames={handleMyGames}
-            handleMatches={handleMatches}
-          />
+          <NavLinks history={this.props.history} handleMyGames={this.props.handleMyGames} />
         </Menu.Item>
         <Menu.Menu position="right">
+
           <Menu.Item>
             <LogIn setUserState={setUserState} />
           </Menu.Item>
           <Menu.Item>
+
           </Menu.Item>
           <Menu.Item
             name="logout"
@@ -102,14 +48,10 @@ export default class Nav extends Component {
           />
         </Menu.Menu>
       </Menu>
->>>>>>> master
     );
   }
 }
 
-<<<<<<< HEAD
-
-=======
 Nav.propTypes = {
   handleSearch: PropTypes.func.isRequired,
   handleMyGames: PropTypes.func.isRequired,
@@ -117,4 +59,3 @@ Nav.propTypes = {
   setUserState: PropTypes.func.isRequired,
   history: PropTypes.object.isRequired,
 };
->>>>>>> master
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -16,10 +16,6 @@ class Search extends Component {
     this.setState({
       [event.target.id]: event.target.value,
     });
-<<<<<<< HEAD
-    // console.log(this.state);
-=======
->>>>>>> master
   }
 
   render() {
@@ -27,39 +23,6 @@ class Search extends Component {
     const { handleSearch } = this.props;
 
     return (
-<<<<<<< HEAD
-      <form
-        className="form-search"
-        value={this.state.platform}
-        id="platform"
-        onChange={this.handleChange}
-        onSubmit={this.handleSubmit}
-      >
-        <select name="platform">
-          <option value="all">All Platforms</option>
-          <option value="PS4">PS4</option>
-          <option value="XBox One">XBox One</option>
-          <option value="Switch">Switch</option>
-        </select>
-        <input
-          type="text"
-          className="inline"
-          placeholder="Search"
-          id="searchTerm"
-          value={this.state.searchTerm}
-          onChange={this.handleChange}
-          required
-          // autoFocus
-        />
-        <button
-          className="btn btn-primary btn-sm"
-          type="submit"
-          // id="loginSubmit"
-        >
-        Search
-        </button>
-      </form>
-=======
       <Route
         render={({ history }) => (
           <form
@@ -99,7 +62,6 @@ class Search extends Component {
           </form>
         )}
       />
->>>>>>> master
     );
   }
 }
