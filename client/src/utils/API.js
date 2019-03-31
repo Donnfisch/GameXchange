fb5f3ddf82be74b734af4304d44d96ac2b60537e
1701c <alex@reventlow.net>
2019-03-31 16:02:44 -0500

    api cleanup

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,36 +1,38 @@
 import axios from 'axios';
 
+const formatHeader = (token) => (
+  {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  });
+
 export default {
   // Gets user data from cookie to update state
-  getUserInfo: () => {
+  formatHeader: (token) => (
+    {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    }),
+
+  getUserInfo: async () => {
     const token = document.cookie.split('; ')
       .filter(
         (element) => element.indexOf('token=') === 0
       )[0].split('=')[1];
-    return axios
-      .get('/api/user/', {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then(res => {
-        res.data.token = token;
-        return (res.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    try {
+      const res = await axios
+        .get('/api/user/', { headers: formatHeader(token) });
+      res.data.token = token;
+      return (res.data);
+    } catch (error) {
+      console.log(error);
+    }
+    return (null);
   },
 
   // Gets user's matches
   getMatches: (token, direction) => axios
-    .get(`/api/inventory/match/${direction}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    .get(`/api/inventory/match/${direction}`, { headers: formatHeader(token) })
     .then(res => (res.data))
     .catch(error => {
       console.log(error);
@@ -43,12 +45,7 @@ export default {
       want,
       trade,
       gameId: boxId,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    }, { headers: formatHeader(token) })
     .then(res => (res.data))
     .catch(error => {
       console.log(error);
@@ -56,12 +53,7 @@ export default {
 
   // Returns games from user's inventory
   getUserGames: (token) => axios
-    .get('/api/inventory/', {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    .get('/api/inventory/', { headers: formatHeader(token) })
     .then(res => (res.data))
     .catch(error => {
       console.log(error);
@@ -69,12 +61,7 @@ export default {
 
   // Searches for games
   searchGames: (token, searchTerm) => axios
-    .get(`/api/games/title/${searchTerm}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    })
+    .get(`/api/games/title/${searchTerm}`, { headers: formatHeader(token) })
     .then(res => (res.data))
     .catch(error => {
       console.log(error);
