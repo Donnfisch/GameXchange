3f0146e0b5191ef1939960b2f69f3caa526de534
1701c <alex@reventlow.net>
2019-04-02 12:08:56 -0500

    profile page, salty password updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,8 +10,24 @@ module.exports = {
     const user = jwt.verify(token, jwtSecret);
     db.user.findOne({
       where: { id: user.id },
-      attributes: ['id', 'username', 'email', 'firstname', 'lastname'],
+      attributes: ['id', 'username', 'email', 'firstname', 'lastname', 'address'],
     })
+      .then(userinfo => {
+        res.json(userinfo);
+      });
+  },
+
+  update: (req, res) => {
+    const token = req.headers.authorization.replace('Bearer ', '');
+    const user = jwt.verify(token, jwtSecret);
+    console.log(`token: ${token}`);
+    console.log(`req.body: ${JSON.stringify(req.body)}`);
+    db.user.update(
+      {
+        email: req.body.email, firstname: req.body.firstname, lastname: req.body.lastname, address: req.body.address, password: req.body.password,
+      },
+      { where: { id: user.id } }
+    )
       .then(userinfo => {
         res.json(userinfo);
       });
